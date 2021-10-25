29c8a02b76917063f132f84615e5bb29f0f6b55c
tenx96 <heyitstenx@gmail.com>
2021-10-25 23:34:58 +0530

    changed buttons on navbar, routing with history

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,26 +1,49 @@
-import React from 'react'
-import "./Navbar.css"
+import { Button, Icon } from "@mui/material";
+import React from "react";
+import { useHistory } from "react-router-dom";
+import "./Navbar.css";
 
 function Navbar({ title, routes }) {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-fixed-top navbar-dark">
-            <div className="container-fluid px-0" style={{overflowX:'hidden'}}>
-                <a className="navbar-brand" href="/">{title}</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                        {routes.map((item , index) => (
-                            <li key={index} className="nav-item">
-                                <a className="nav-link active" aria-current="page" href={item.route}>{item.label}</a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </nav>
-    )
+  const history = useHistory();
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-fixed-top navbar-dark">
+      <div className="container-fluid px-0" style={{ overflowX: "hidden" }}>
+        <a className="navbar-brand" href="/">
+          {title}
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <Icon>menu</Icon>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            {routes.map((item, index) => (
+              <Button
+                key={index}
+                variant="text"
+                onClick={() => {
+                  history.push(item.route);
+                }}
+                sx={{
+                  color: "white",
+                }}
+              >
+                {item.label}
+              </Button>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
