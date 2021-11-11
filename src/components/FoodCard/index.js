a12f16e3f0c7b41ef5d9803ac87ea43817cdd99b
Biju Borah <biju20_ug@ee.nits.ac.in>
2021-11-12 00:23:50 +0530

    fetch data via API

---
+++ src/components/FoodCard/index.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+function FoodCard(props) {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title={props.food['title']}
+                subheader={props.food['subtitle']}
+            />
+            <CardMedia
+                component="img"
+                height="194"
+                image={props.food['image']}
+                alt="Paella dish"
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {props.food['desc']}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default FoodCard
--- src/routes/Food/index.js
+++ src/routes/Food/index.js
@@ -1,9 +1,36 @@
 import ComingSoon from 'components/ComingSoon'
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from 'react'
+import FoodCard from 'components/FoodCard'
+import { Grid } from "@mui/material";
 export const ROUTE_FOOD = "/food"
+
+
 function Food() {
+    const fetchData = () => {
+        return axios.get("https://5db305d7a394f5001443a97d.mockapi.io/api/v1/food")
+              .then((response) => { 
+                console.log(response.data)
+                setlist(response.data)
+              })}
+
+    const [list, setlist] = useState({})
+    useEffect(() => {
+        fetchData();
+        }, []);
     return (
-        <ComingSoon></ComingSoon>
+    <>
+        {Object.keys(list).length > 0 ? <div className="food-list" style={{marginTop:'64px'}}>
+        <Grid justifyContent="center" container spacing={4}>
+        {list['food'].map((item, i) => (
+            <Grid item md={3} xs={12}>
+                <FoodCard food={item} key={i}/>
+            </Grid>
+        ))}
+        </Grid>
+        </div> : <ComingSoon></ComingSoon>}
+        
+    </>
     )
 }
 
